b7a0970cce588b9e0cc20fbe0eebd0c210edfb71
Muhammed faiz kk <fayizkk5@gmail.com>
2024-12-09 10:25:35 +0530

    changedadding

--- middleware/fileUpload.js
+++ middleware/fileUpload.js
@@ -8,18 +8,15 @@ cloudinary.config({
     api_key: 968835199376537,
     api_secret: 'kCOm5DSVGymQ85gJJ7nsdJayWZI',
 });
-
-// Set up Multer to upload directly to Cloudinary and allow all image formats
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
-        folder: 'capstone',  // Specify your folder in Cloudinary
+        folder: 'agi',
         allowed_formats: ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'webp', 'svg', 'tiff'],  // Allowed image formats
         public_id: (req, file) => file.originalname.split('.')[0] + '-' + Date.now(),  // Create a unique name for the image
     },
 });
 
-// Multer configuration
 const upload = multer({ storage });
 
 module.exports = upload;
