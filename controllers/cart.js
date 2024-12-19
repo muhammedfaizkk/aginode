5a0ffbf2da8a4b7c1cb51a499cb654271873aca8
Muhammed faiz kk <fayizkk5@gmail.com>
2024-12-19 18:41:58 +0530

    changedcarts

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,4 +1,5 @@
 const Cart = require("../models/cartmodel");
+const Product = require('../models/ProudctModel')
 
 exports.addToCart = async (req, res) => {
     try {
@@ -105,21 +106,39 @@ exports.clearCart = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
-
-
 exports.getCart = async (req, res) => {
     try {
-        const { userId } = req.query;
+        const { userId } = req.query; // Extract the user ID from the query parameters
 
+        // Find the cart for the user and populate the product details in items
         const cart = await Cart.findOne({ user: userId }).populate("items.product");
 
+        // If no cart exists, return a 404 response
         if (!cart) {
             return res.status(404).json({ message: "Cart not found" });
         }
 
-        res.status(200).json({ success: true, cart });
+        // Extract the products from the cart items
+        const cartedProducts = cart.items.map(item => ({
+            productId: item.product._id,
+            name: item.product.name,
+            price: item.product.price,
+            quantity: item.quantity,
+            total: item.quantity * item.product.price,
+            image: item.product.images?.[0], // Send the first image, if available
+        }));
+
+        // Send the cart and the carted products in the response
+        res.status(200).json({
+            success: true,
+            cartId: cart._id,
+            user: cart.user,
+            totalQuantity: cart.items.reduce((acc, item) => acc + item.quantity, 0), // Total quantity of items
+            totalPrice: cartedProducts.reduce((acc, item) => acc + item.total, 0), // Total price of items
+            products: cartedProducts,
+        });
     } catch (error) {
+        // Send a 500 response in case of a server error
         res.status(500).json({ message: error.message });
     }
 };
-
