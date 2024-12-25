86280a5511bda9837cb5d765b5ba4a1aaf0e633a
Muhammed faiz kk <fayizkk5@gmail.com>
2024-12-25 13:18:26 +0530

    databaseconneci

--- config/databaseConnection.js
+++ config/databaseConnection.js
@@ -1,15 +1,9 @@
 const mongoose = require("mongoose");
 
-const databaseConnection = async () => {
-  try {
-    const connection = await mongoose.connect(process.env.DB_URI, {
-      serverSelectionTimeoutMS: 50000, // 50 seconds
-      connectTimeoutMS: 50000,         // 50 seconds
-    });
-    console.log(`Database is connected with ${connection.connection.host}`);
-  } catch (err) {
-    console.error(`Database connection error: ${err.message}`);
-  }
-};
+const databaseConnection = ()=>{
+     mongoose.connect(process.env.DB_URI)
+    .then((data) => console.log(`Database is connected with ${data.connection.host}`))
+    .catch((err) => console.log(err.message))
+}
 
 module.exports = databaseConnection;
