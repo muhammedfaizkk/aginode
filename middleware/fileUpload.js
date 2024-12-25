5d233f90b729148aaca302abf5f630ef8e8bb435
Muhammed faiz kk <fayizkk5@gmail.com>
2024-12-25 13:53:24 +0530

    databaseconne

--- middleware/fileUpload.js
+++ middleware/fileUpload.js
@@ -1,4 +1,5 @@
 const multer = require('multer');
+const path = require('path');
 
 // Set storage configuration for the uploaded files
 const storage = multer.diskStorage({
@@ -6,7 +7,7 @@ const storage = multer.diskStorage({
         cb(null, 'uploads/');  // Directory where files will be stored
     },
     filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname);  // Unique filename
+        cb(null, Date.now() + '-' + file.originalname);  // Unique filename to avoid overwriting
     },
 });
 
@@ -17,9 +18,9 @@ const upload = multer({
         fileSize: 3 * 1024 * 1024,  // Max file size 3MB
     },
     fileFilter: (req, file, cb) => {
-        const filetypes = /jpeg|jpg|png|gif|webp/;  // Added 'webp' to the allowed file types
+        const filetypes = /jpeg|jpg|png|gif|webp/;  // Allowed file types
         const mimeType = filetypes.test(file.mimetype);
-        const extname = filetypes.test(file.originalname.toLowerCase());
+        const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
 
         if (mimeType && extname) {
             return cb(null, true);  // Accept the file
