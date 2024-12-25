d9a948896a2070f70bf191f1548fe265bb42483a
Muhammed faiz kk <fayizkk5@gmail.com>
2024-12-25 12:30:16 +0530

    databaseconnecitonfixed

--- config/databaseConnection.js
+++ config/databaseConnection.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
 
-const databaseConnection = ()=>{
-     mongoose.connect(process.env.DB_URI)
-    .then((data) => console.log(`Database is connected with ${data.connection.host}`))
-    .catch((err) => console.log(err.message))
-}
+const databaseConnection = () => {
+    mongoose.connect(process.env.DB_URI, {
+        useNewUrlParser: true,    // Ensures the use of the new URL parser
+        useUnifiedTopology: true, // Ensures the use of the new server discovery and monitoring engine
+    })
+    .then((data) => {
+        console.log(`Database is connected with ${data.connection.host}`);
+    })
+    .catch((err) => {
+        console.error("Database connection error:", err.message);
+        process.exit(1); // Exit the process if the database connection fails
+    });
+};
 
 module.exports = databaseConnection;
