593105cf59c6db96d69aecf51a09f15b78591565
Muhammed faiz kk <fayizkk5@gmail.com>
2024-12-25 12:41:12 +0530

    databaseconnecitonfe

--- config/databaseConnection.js
+++ config/databaseConnection.js
@@ -1,9 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const databaseConnection = () => {
   mongoose.connect(process.env.DB_URI, {
-    serverSelectionTimeoutMS: 5000,  // Increase the timeout as needed
-    connectTimeoutMS: 10000,         // Increase the connection timeout
+    serverSelectionTimeoutMS: 15000, // 15 seconds
+    connectTimeoutMS: 15000,         // 15 seconds
+    useNewUrlParser: true,           // Use the new parser (although deprecated, still supported for now)
+    useUnifiedTopology: true        // Use the new topology engine
   })
   .then((data) => {
     console.log(`Database is connected with ${data.connection.host}`);
