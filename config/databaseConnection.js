fdb22b8c1a3c3f22c4e349d6825d44eb2e206aaf
Muhammed faiz kk <fayizkk5@gmail.com>
2024-12-26 10:01:39 +0530

    timout issue solved secondss

--- config/databaseConnection.js
+++ config/databaseConnection.js
@@ -1,16 +1,9 @@
 const mongoose = require("mongoose");
 
-const databaseConnection = () => {
-    mongoose.connect(process.env.DB_URI, {
-        connectTimeoutMS: 30000,  
-        socketTimeoutMS: 30000,  
-    })
-    .then((data) => {
-        console.log(`Database connected successfully: ${data.connection.host}:${data.connection.port}`);
-    })
-    .catch((err) => {
-        console.error('Database connection error:', err.message);
-    });
-};
+const databaseConnection = ()=>{
+     mongoose.connect(process.env.DB_URI)
+    .then((data) => console.log(`Database is connected with ${data.connection.host}`))
+    .catch((err) => console.log(err.message))
+}
 
 module.exports = databaseConnection;
