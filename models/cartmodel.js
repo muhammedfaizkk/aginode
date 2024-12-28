e062b6bc3f08e75ef685508559466b85759c9ff3
Muhammed faiz kk <fayizkk5@gmail.com>
2024-12-28 16:52:04 +0530

    addedbannscarts

--- models/cartmodel.js
+++ models/cartmodel.js
@@ -1,31 +1,32 @@
 const mongoose = require("mongoose");
-const Product = require("./ProudctModel"); 
 
 const cartSchema = new mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User", // Reference to the User model
+      ref: "User",
       required: true,
     },
     items: [
       {
         product: {
           type: mongoose.Schema.Types.ObjectId,
-          ref: "Product", // Reference to the Product model
+          ref: "Product",
           required: true,
         },
         quantity: {
           type: Number,
           required: true,
-          min: [1, "Quantity must be at least 1"], // Validation for minimum quantity
+          min: [1, "Quantity must be at least 1"],
         },
       },
     ],
   },
   {
-    timestamps: true, // Automatically includes createdAt and updatedAt fields
+    timestamps: true,
   }
 );
 
 const Cart = mongoose.model("Cart", cartSchema);
+
+module.exports = Cart;
