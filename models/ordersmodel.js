48239b75b947fa606541d8d3ac8df4477762aaa3
Muhammed faiz kk <fayizkk5@gmail.com>
2025-01-23 23:27:01 +0530

    changedgitssddfalse

--- models/ordersmodel.js
+++ models/ordersmodel.js
@@ -10,7 +10,7 @@ const orderSchema = new mongoose.Schema(
         user: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "User",
-            required: true,
+            required: false,
         },
         products: [
             {
