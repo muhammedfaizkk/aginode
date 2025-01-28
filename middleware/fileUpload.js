febc676955dd483056ab0347c3ee09f8df6eaf63
Muhammed faiz kk <fayizkk5@gmail.com>
2025-01-28 12:28:59 +0530

    addedsharpmulter

--- middleware/fileUpload.js
+++ middleware/fileUpload.js
@@ -1,7 +1,7 @@
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-
+const sharp = require('sharp');
 
 const uploadsPath = path.resolve(__dirname, '../uploads');
 
@@ -13,14 +13,25 @@ const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, uploadsPath);
     },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname);
+    filename: async (req, file, cb) => {
+        const fileName = Date.now() + '-' + file.originalname.split('.')[0] + '.webp'; // Convert to webp format
+        const filePath = path.join(uploadsPath, fileName);
+
+        // Convert and save the file as .webp
+        sharp(file.buffer)
+            .webp()
+            .toFile(filePath, (err, info) => {
+                if (err) {
+                    return cb(err);
+                }
+                cb(null, fileName); // Pass the new filename
+            });
     },
 });
 
 const upload = multer({
     storage,
-    limits: { fileSize: 3 * 1024 * 1024 },
+    limits: { fileSize: 3 * 1024 * 1024 }, // 3MB limit
     fileFilter: (req, file, cb) => {
         const filetypes = /jpeg|jpg|png|gif|webp/;
         const mimeType = filetypes.test(file.mimetype);
